657cc19e2b448e7c6bccaeb5f37f386b41da250b
khanya jara <khanyajara@gmail.com>
2024-11-01 12:44:22 +0200

    api recipe app was working , now trying to fix auth error , pushing with no other errors

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connected to cluster: ${mongoose.connection.host}`);
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
